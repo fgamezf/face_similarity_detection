9d39f9b604848531241279def17909612990c131
sxj731533730 <sxj731533730@gmail.com>
2021-11-06 20:11:27 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -47,7 +47,7 @@ class CvFpsCalc(object):
 
 def drawLandmarks(image, results):
     # 1. Draw face landmarks
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS, 
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
         mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
         mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
         )
